70c7ab2cb340752b1e6791a7419d9f18cced253b
Iagocanalejas <iagocanalejas@gmail.com>
2025-04-02 12:54:02 +0200

    BATMAN

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="flex items-center justify-between p-4 bg-gray-800 text-white shadow-md">
+        <div class="flex items-center space-x-4">
+            <img src="@/assets/logo.svg" alt="Logo" class="h-10 w-10" />
+        </div>
+        <nav class="flex space-x-4 ml-auto">
+            <RouterLink
+                to="/"
+                class="px-3 py-2 rounded-lg transition duration-200 hover:bg-gray-700 hover:text-gray-300"
+            >
+                Portfolio
+            </RouterLink>
+            <RouterLink
+                to="/shares"
+                class="px-3 py-2 rounded-lg transition duration-200 hover:bg-gray-700 hover:text-gray-300"
+            >
+                Market Shares
+            </RouterLink>
+            <RouterLink
+                to="/indexes"
+                class="px-3 py-2 rounded-lg transition duration-200 hover:bg-gray-700 hover:text-gray-300"
+            >
+                Indexes
+            </RouterLink>
+        </nav>
+    </header>
+    <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
