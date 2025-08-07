65b7038474dab80eed3e679c3f4787938687ebc2
Iagocanalejas <iagocanalejas@gmail.com>
2025-08-07 12:27:50 +0200

    transactions are now filtered by account in the transactions view

--- web/src/views/TransactionsView.vue
+++ web/src/views/TransactionsView.vue
@@ -26,6 +26,7 @@ import ConfirmationModal from '@/components/modals/ConfirmationModal.vue';
 import TransactionModal from '@/components/modals/TransactionModal.vue';
 import SearchComponent from '@/components/SearchComponent.vue';
 import TransactionsListComponent from '@/components/transactions/TransactionsListComponent.vue';
+import { useSettingsStore } from '@/stores/settings';
 import { useTransactionsStore } from '@/stores/transactions';
 import type { TransactionItem } from '@/types/portfolio';
 import { storeToRefs } from 'pinia';
@@ -34,10 +35,13 @@ import { computed, ref } from 'vue';
 const transactionsStore = useTransactionsStore();
 const { updateTransaction, removeTransaction } = transactionsStore;
 const { transactions } = storeToRefs(transactionsStore);
+const { account: selectedAccount } = storeToRefs(useSettingsStore());
 
 const query = ref('');
 const filteredTransactions = computed(() =>
-    transactions.value.filter((tx) => tx.symbol.ticker.toLowerCase().includes(query.value.toLowerCase()))
+    transactions.value
+        .filter((tx) => !selectedAccount.value?.id || tx.account?.id === selectedAccount.value.id)
+        .filter((tx) => tx.symbol.ticker.toLowerCase().includes(query.value.toLowerCase()))
 );
 
 const confirmationModal = ref<InstanceType<typeof ConfirmationModal> | null>(null);
